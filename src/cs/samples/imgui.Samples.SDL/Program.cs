1b0db46d3b7b27038c4a72db23f409958f127054
Lucas Girouard-Stranks <lustranks@gmail.com>
2021-07-28 22:01:24 -0400

    .NET scaffold

---
+++ src/cs/samples/imgui.Samples.SDL/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Diagnostics;
+
+namespace imgui.Samples.SDL
+{
+    internal static class Program
+    {
+	    private static void Main()
+        {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
